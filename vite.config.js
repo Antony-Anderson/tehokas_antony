3dde8dd11d36164e70efee34dcead3685c16fb24
Antony-Anderson <antony01anderson@gmail.com>
2026-09-19 10:53:14 -0300

    configurando docker para salvar automaticamente as alterações

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,18 @@ export default defineConfig({
         tailwindcss(),
         react(),
     ],
+
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+
         watch: {
             ignored: ['**/storage/framework/views/**'],
+        },
+
+        hmr: {
+            host: 'localhost',
+            port: 5173,
         },
     },
 });
